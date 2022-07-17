261472b6744c99c270c45723a000f8ebcc021848
Ayberg Oguz <h.aybergoguz@gmail.com>
2022-07-17 08:35:47 +0300

    anlayamama durmumunda yorum satirlari kaldirilip asama asama takip edilebilinir

--- Uyglama1.java
+++ Uyglama1.java
@@ -5,12 +5,12 @@ public class Uyglama1 {
     static boolean isPalindrom(int number){
         int temp=number,reverseNumber=0,lastnumber;
         while(temp !=0){
-            System.out.println("==============");
-            System.out.println("Sayi :"+temp);
+            //System.out.println("==============");
+            //System.out.println("Sayi :"+temp);
             lastnumber= temp %10;
-            System.out.println("Son Basamak:"+lastnumber);
+            //System.out.println("Son Basamak:"+lastnumber);
             reverseNumber=reverseNumber*10 +lastnumber;
-            System.out.println("Yeni sayi:"+reverseNumber);
+            //System.out.println("Yeni sayi:"+reverseNumber);
             temp/=10;
         }
         if(number==reverseNumber)
