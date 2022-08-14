b8d403241f9b619a50ace68276eac46574d48970
Ayberg Oguz <h.aybergoguz@gmail.com>
2022-08-14 13:11:30 +0300

    harmonik

--- Dizi03.java
+++ Dizi03.java
@@ -1,15 +1,25 @@
+import java.net.SocketTimeoutException;
+
 public class Dizi03{
     public static void main(String[] args) {
 
-        int[] list = {3,5,2,3,7,9,4,2,15,13,24};
+        int[] list = {1,2,3,4};
         double avarage=0.0;
         double sum=0.0;
-        for(int i=0; i<list.length-1;i++){
+        for(int i=0; i<list.length;i++){
             sum+=list[i];
         }
         avarage= sum / list.length;
         System.out.println(sum);
         System.out.println(avarage);
+
+        double sum1=0.0;
+        for(int i=0; i<list.length;i++){
+            sum1 += (1.0 / list[i]);
+        }
+        double harmonik= list.length / sum1;
+        System.out.println(harmonik);
+
 
         
     }
